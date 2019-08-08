bd1b9cb23a249757dbd0fae703c0e8f6ee1030ed
Yunly International <yunlyinternational@gmail.com>
2019-08-08 19:10:46 +0530

    Initial commit

--- src/meterUI/AppUi.java
+++ src/meterUI/AppUi.java
@@ -8,6 +8,9 @@ package meterUI;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
@@ -163,6 +166,9 @@ public class AppUi extends javax.swing.JFrame {
 
         //----------------------------------------- check java c++
         //if(jRadioButton1.)
+        String code = jTextArea1.getText();
+        System.out.println(countLines(code));
+        chechCsKeyWordsJava(code);
         //-----------------------------------------run
         //-----------------------------------------
         //-----------------------------------------
@@ -197,6 +203,317 @@ JFileChooser chooser = new JFileChooser();
             JOptionPane.showMessageDialog(null, e);
         }        // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    
+//check how many lines in the code use to create the result array
+    public int countLines(String code){
+        Matcher m = Pattern.compile("\r\n|\r|\n").matcher(code);
+        int lines = 1;
+        while (m.find())
+        {
+            lines ++;
+        }
+    return lines;
+    }
+    
+    public void chechCsKeyWordsJava(String code){        
+        //ArrayList<String> code_array = new ArrayList<>();
+        Scanner scanner = new Scanner(code);
+                
+        String text = code;        
+        
+        int arraySize = countLines(code);  
+        
+        String CsKeys[] = new String[arraySize];
+        int Cs[] = new int[arraySize];
+
+        for(int i = 0; i<arraySize; i++){
+                    Cs[i] = 0;
+                    CsKeys[i] = " ";
+        }
+                
+
+        String[] lines = text.split("\\r?\\n");        
+        int i=0;
+        for(String line : lines){
+            
+            if(line.contains("&")){
+                CsKeys[i] = CsKeys[i] + "& ";
+                Cs[i] = Cs[i] + 2;
+            }
+            if(line.contains("public")){
+                CsKeys[i] = CsKeys[i] + "public ";
+                Cs[i] = Cs[i] + 2;
+            }
+            if(line.contains("new")){
+                CsKeys[i] = CsKeys[i] + "new ";
+                Cs[i] = Cs[i] + 2;
+            }
+            if(line.contains("delete")){
+                CsKeys[i] = CsKeys[i] + "delete ";
+                Cs[i] = Cs[i] + 2;
+            }
+            if(line.contains("throw")){
+                CsKeys[i] = CsKeys[i] + "throw ";
+                Cs[i] = Cs[i] + 2;
+            }
+            if(line.contains("throws")){
+                CsKeys[i] = CsKeys[i] + "throws ";
+                Cs[i] = Cs[i] + 2;
+            }
+            if(line.contains("void")){
+                CsKeys[i] = CsKeys[i] + "void ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("double")){
+                CsKeys[i] = CsKeys[i] + "double ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("int")){
+                CsKeys[i] = CsKeys[i] + "int ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("float")){
+                CsKeys[i] = CsKeys[i] + "float ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("string")){
+                CsKeys[i] = CsKeys[i] + "string ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("printf")){
+                CsKeys[i] = CsKeys[i] + "printf ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("println")){
+                CsKeys[i] = CsKeys[i] + "println ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("print")){
+                CsKeys[i] = CsKeys[i] + "print ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("cout")){
+                CsKeys[i] = CsKeys[i] + "cout ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("cin")){
+                CsKeys[i] = CsKeys[i] + "cin ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("if’")){
+                CsKeys[i] = CsKeys[i] + "if ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("for")){
+                CsKeys[i] = CsKeys[i] + "for ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("while")){
+                CsKeys[i] = CsKeys[i] + "while ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("do")){
+                CsKeys[i] = CsKeys[i] + "do ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("switch")){
+                CsKeys[i] = CsKeys[i] + "switch ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("case")){
+                CsKeys[i] = CsKeys[i] + "case ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("endl’")){
+                CsKeys[i] = CsKeys[i] + "endl ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("'")){
+                CsKeys[i] = CsKeys[i] + "' ";
+                Cs[i] = Cs[i] + 1;
+            }
+            if(line.contains("+")){
+                if(line.contains("++")){
+                CsKeys[i] = CsKeys[i] + "++ ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("+=")){
+                CsKeys[i] = CsKeys[i] + "+= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "+ ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("-")){
+                if(line.contains("--")){
+                CsKeys[i] = CsKeys[i] + "-- ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("->")){
+                CsKeys[i] = CsKeys[i] + "-> ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("-=")){
+                CsKeys[i] = CsKeys[i] + "-= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "- ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("=")){
+                if(line.contains("==")){
+                CsKeys[i] = CsKeys[i] + "== ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "= ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("!")){
+                if(line.contains("!=")){
+                CsKeys[i] = CsKeys[i] + "!= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "! ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains(">")){
+                if(line.contains(">=")){
+                CsKeys[i] = CsKeys[i] + ">= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains(">>=")){
+                CsKeys[i] = CsKeys[i] + ">>= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains(">>>=")){
+                CsKeys[i] = CsKeys[i] + ">>>= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains(">>>")){
+                CsKeys[i] = CsKeys[i] + ">>> ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains(">>")){
+                CsKeys[i] = CsKeys[i] + ">> ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "> ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("<")){
+                if(line.contains("<=")){
+                CsKeys[i] = CsKeys[i] + "<= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("<<=")){
+                CsKeys[i] = CsKeys[i] + "<<= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("<<<=")){
+                CsKeys[i] = CsKeys[i] + "<<<= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("<<<")){
+                CsKeys[i] = CsKeys[i] + "<<< ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else if(line.contains("<<")){
+                CsKeys[i] = CsKeys[i] + "<< ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "< ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("*")){
+                if(line.contains("*=")){
+                CsKeys[i] = CsKeys[i] + "*= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                //code for poiner
+                //else if(line.contains("**")){
+                //CsKeys[i] = CsKeys[i] + "** ";
+                //Cs[i] = Cs[i] + 1;
+                //}
+                else{
+                CsKeys[i] = CsKeys[i] + "* ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("/")){
+                if(line.contains("/=")){
+                CsKeys[i] = CsKeys[i] + "/= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "/ ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("|")){
+                if(line.contains("|=")){
+                CsKeys[i] = CsKeys[i] + "|= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "| ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("%")){
+                if(line.contains("%=")){
+                CsKeys[i] = CsKeys[i] + "%= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "% ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains(":")){
+                if(line.contains("::")){
+                CsKeys[i] = CsKeys[i] + ":: ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + ": ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            if(line.contains("^")){
+                if(line.contains("^=")){
+                CsKeys[i] = CsKeys[i] + "^= ";
+                Cs[i] = Cs[i] + 1;
+                }
+                else{
+                CsKeys[i] = CsKeys[i] + "^ ";
+                Cs[i] = Cs[i] + 1;
+                }
+            }
+            
+            
+            
+            System.out.print(CsKeys[i] + "      ");
+            System.out.println(Cs[i]);
+
+            i = i + 1;
+        }   
+
+       
+    }
+
 
     private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
 
